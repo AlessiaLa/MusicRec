0d4896bd016fac0922ca35acee23243ce3773e9b
Alessia Laforgia <84467216+AlessiaLa@users.noreply.github.com>
2022-03-06 18:07:46 +0100

    added modularity csv2json

--- csv2json.py
+++ csv2json.py
@@ -2,37 +2,41 @@
 import re
 import json
 
+pattern_artists = '(?P<artistname>.*), (?P<artistgenres>\[.*]), (?P<albumids>\[.*])'
+pattern_albums = '(?P<albumid>.*), (?P<albumname>.*), (?P<artistnames>\[.*]), (?P<trackids>\[.*])'
+pattern_tracks = '(?P<trackid>.*), (?P<trackname>.*), (?P<artistnames>\[.*]), (?P<albumname>.*)'
+
 def get_list_from_string(string: str):
     string = string.replace('[', '')
     string = string.replace(']', '')
+    string = string.replace('\"', '')
     if string == '':
         return []
     return string.replace(", ", ",").split(",")
 
 
-pattern = '(?P<artistname>.*), (?P<artistgenres>\[.*]), (?P<albumid>\[.*])'
-regex = re.compile(pattern)
-artist_dict = {}
-artist_list = []
-
-with open('artists.txt', encoding='utf-8') as f:
-    with open('artists.json', 'w') as fp:
-        json.dump(artist_dict, fp)
-        for line in f:
-            stripped_line = line.strip()
-
-            result = regex.match(str(line))
-            d = result.groupdict()
-            artist_list.append(d)
-            print(d)
-            d['artistgenres'] = get_list_from_string(d['artistgenres'])
-            d['albumid'] = get_list_from_string(d['albumid'])
-            json.dump(d, fp)
-        #artist_dict['artist'] = artist_list
+def csv2json(csv, pattern, json_filename):
+    dict = {}
+    list = []
+    regex = re.compile(pattern)
+    with open(csv, encoding='utf-8') as f:
+        with open(json_filename, 'w') as fp:
+            json.dump(dict, fp)
+            for line in f:
+                stripped_line = line.strip()
+                result = regex.match(str(stripped_line))
+                d = result.groupdict()
+                list.append(d)
+                keys_list = d.keys()
+                for k in keys_list:
+                    print (k)
+                    d[k] = get_list_from_string(d[k])
+                json.dump(d, fp)
 
 
-#print(artist_dict)
-#with open('artists.json', 'w') as fp:
-    #json.dump(artist_dict, fp)
 
 
+if __name__ == "__main__":
+    csv2json('artists.txt', pattern_artists, 'artists.json')
+    csv2json('albums.txt', pattern_albums, 'albums.json')
+    csv2json('tracks.txt', pattern_tracks, 'tracks.json')

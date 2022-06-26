7ac6dec066ef7a5534d00152672cc5b97bf3a08b
Daniela Grassi <daniela.grassi.98@gmail.com>
2022-06-26 12:39:24 +0200

    put absolute path

--- kbAccess.py
+++ kbAccess.py
@@ -1,8 +1,14 @@
-from pyswip import Prolog
+import os
 
+from pyswip import Prolog
+os.chdir(os.path.join(os.getcwd(), "prolog"))
+
+
+path = os.path.join(os.getcwd(), "album.pl").replace("\\","/")
+print(path)
 prolog = Prolog()
 
-prolog.consult("C:/Users/Alessia/Documents/GitHub/MusicNet/prolog/album.pl")
+prolog.consult(path)
 
 c = bool(list(prolog.query("album(X,\"cleopatra\")")))
 print(c)

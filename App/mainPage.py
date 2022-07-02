26736de5248299b25cd21f78ec003119470261e0
Alessia Laforgia <84467216+AlessiaLa@users.noreply.github.com>
2022-07-02 14:48:40 +0200

    trying to implement session state

--- App/mainPage.py
+++ App/mainPage.py
@@ -2,55 +2,48 @@ import streamlit as st
 import pandas as pd
 import base64
 import threading
-from openpage import threadFunc
 import os
+import menu
 
-def App1page(session,p1,p2,p3,p4):
-    if session==1:
-        p1.empty()
-        p2.empty()
-        p3.empty()
-        p4.empty()
-    placeholder_module = st.empty()
-    placeholder_module = st.markdown("<h1 style='text-align: center; color: white;'>Ciao  </h1>"
-                "<h1 style='text-align: center; color: red;'>MusicRec!</h1>", unsafe_allow_html=True)
+"st.session_state object:", st.session_state
 
 def main_page():
-    placeholder_main1=st.empty()
-    placeholder_main2=st.empty()
-    placeholder_main3=st.empty()
-    placeholder_main4 = st.empty()
-    placeholder_main1.markdown("<h1 style='text-align: center; color: white;'>Welcome to </h1>"
-                "<h1 style='text-align: center; color: red;'>MusicRec!</h1>", unsafe_allow_html=True)
-    #st.markdown("<h1 style='text-align: center; color: red;'>MusicRec!</h1>", unsafe_allow_html=True)
+    clicked = False
+    interface1=st.container()
+    with interface1:
+        l, m, r = interface1.columns(3)
+        with m:
+            placeholder_main1 = st.markdown("<h1 style='text-align: center; color: white;'>Welcome to </h1>"
+                                            "<h1 style='text-align: center; color: red;'>MusicRec!</h1>",
+                                            unsafe_allow_html=True)
 
-    _left, lm, mid, mr, _right = st.columns(5)
-    with mid:
-        placeholder_main2.markdown("![Alt Text](https://media.giphy.com/media/tqfS3mgQU28ko/giphy.gif)")
-        placeholder_main3.markdown("<h2 style='text-align: center; color: white;'>Ready to dive into music with me? 🙌 </h2>", unsafe_allow_html=True)
-        clicked = placeholder_main4.button("Start")
-    #st.markdown(f'<h1 style="color:#33ff33;font-size:24px;">{}</h1>', unsafe_allow_html=True)
+    interface2=st.container()
+    with interface2:
+        a,b,c,d,e,f=st.columns(6)
+        with b:
+            placeholder_main2 = st.markdown("![Alt Text](https://media.giphy.com/media/tqfS3mgQU28ko/giphy.gif)",
+                                            unsafe_allow_html=True)
+        placeholder_main3 = st.markdown(
+            "<h2 style='text-align: center; color: white;'>Ready to dive into music with me? 🙌 </h2>",
+            unsafe_allow_html=True)
 
 
-    #x = st.slider('x')  # 👈 this is a widget
-    #st.write(x, 'squared is', x * x)
-
-    # _left,mid,ls, _right = st.columns(4)
-    # with ls:
-
+    interface3=st.empty()
+    if 'start' not in st.session_state:
+        with interface3:
+                g,h,i,j,k,l,m = st.columns(7)
+                with j:
+                    placeholder_main4 = st.empty()
+                    if placeholder_main4.button("Start"):
+                        clicked=True
+                        st.session_state.start=True
+                        menu.main()
 
     if clicked:
-        session=1
-        # th = threading.Thread(target=threadFunc)
-        # th.start()
-        # # address of streamlit page that you want to open after clicking button
-        # # os.system('App')
-        # # os.system('cd python\Scripts')
-        # #os.system(r"streamlit run App/userdialogue.py")
-        # th.join()
-        App1page(session,placeholder_main1,placeholder_main2,placeholder_main3,placeholder_main4)
-
-
+        placeholder_main1.empty()
+        placeholder_main2.empty()
+        placeholder_main3.empty()
+        placeholder_main4.empty()
 
 
 if __name__ == "__main__":
--- App/menu.py
+++ App/menu.py
@@ -1,8 +1,8 @@
 # Contents of ~/my_app/streamlit_app.py
 import streamlit as st
-from streamlit_option_menu import option_menu
+from streamlit.scriptrunner import RerunException, RerunData
 
-
+"st.session_state object:",st.session_state
 
 def preferences():
     st.write("page preferences")
@@ -13,38 +13,40 @@ def mood():
 def new():
     st.write("try")
 
-page_names = ['Mood', 'Preferences']
+page_names = ('Mood', 'Preferences')
+
+def main():
+    if 'selected' not in st.session_state:
+        with st.sidebar:
+            selected_page = st.sidebar.multiselect("Select a page", page_names)
+            st.session_state.selected=selected_page
+            print(selected_page)
+            if selected_page=="Mood":
+                mood()
+            if selected_page=="Preferences":
+                preferences()
+            else:
+              new()
+
+#"st.session_state object:", st.session_state
 
 
 
 # def main():
-#     selected_page = st.sidebar.multiselect("Select a page", page_names)
-#     print(selected_page)
-#     if selected_page=="Mood":
-#         mood()
-#     if selected_page=="Preferences":
-#         preferences()
-#     else:
-#       new()
+#     st.session_state.dropbox=None
+#     if 'dropbox' not in st.session_state:
+#         with st.sidebar:
+#             st.session_state.dropbox = st.multiselect("Tell me about your...", page_names)
+#             print(st.session_state.dropbox)
+#         if "Preferences" in st.session_state.dropbox:
+#             preferences()
+#
 
 
-def main():
+    # last_y1 = st.session_state.y1
+    # st.session_state.y1 = st.multiselect(
+    #     "Select year(s)",
+    #     ['2013', '2014', '2015', '2016', '2017', '2018', '2019'],
+    #     st.session_state.y1)
+    # print(st.session_state.y1)
 
-        with st.sidebar:
-            choose = option_menu("App Gallery",
-                                 ["About", "Photo Editing", "Project Planning", "Python e-Course", "Contact"],
-                                 icons=['house', 'camera fill', 'kanban', 'book', 'person lines fill'],
-                                 menu_icon="app-indicator", default_index=0,
-                                 styles={
-                                     "container": {"padding": "5!important", "background-color": "#fafafa"},
-                                     "icon": {"color": "orange", "font-size": "25px"},
-                                     "nav-link": {"font-size": "16px", "text-align": "left", "margin": "0px",
-                                                  "--hover-color": "#eee"},
-                                     "nav-link-selected": {"background-color": "#02ab21"},
-                                 }
-                                 )
-            if choose not in st.session_state:
-                st.session_state['page'] = choose
-            print(choose)
-            if choose=="About":
-                preferences()

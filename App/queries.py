cf0ecccfc14962244ace5647a71e1716c5a76a5c
Daniela Grassi <daniela.grassi.98@gmail.com>
2022-07-11 10:57:36 +0200

    add getArtistFromTrack

--- App/queries.py
+++ App/queries.py
@@ -3,7 +3,7 @@ import kbAccess
 
 def getTracksByFeatures(n: int, dance: str, energy: str, valence: str):
     pi = kbAccess.PrologInterface()
-    pi.set_to_consult(['track.pl', 'features.pl', 'utils.pl', 'init.pl'])
+    pi.set_to_consult(['track.pl', 'features.pl', 'utils.pl', 'init.pl', 'tracksuggest.pl'])
     pi.load_rules()
     query = f"getTracksByFeatures({n}, \"{dance}\", \"{energy}\", \"{valence}\", Tracks)."
     result = pi.query(query)
@@ -11,4 +11,14 @@ def getTracksByFeatures(n: int, dance: str, energy: str, valence: str):
 
 
 
-def
+def getArtistFromTrack(tracks: list, album: list):
+    pi = kbAccess.PrologInterface()
+    pi.set_to_consult(['track.pl', 'album_contains.pl', 'utils.pl', 'init.pl', 'published_by.pl', 'album.pl'])
+    pi.load_rules()
+    query = f"retrieveAllArtists({tracks}, {album}, Artists)."
+    result = pi.query(query)
+    return result
+
+
+if __name__ == '__main__':
+    print(getArtistFromTrack(["angela", "boogie woogie baltimore"], ["cleopatra", "null"]))

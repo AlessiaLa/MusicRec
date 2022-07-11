35a837bbc55f157dd677886e5880a8e550ce3e5b
Daniela Grassi <daniela.grassi.98@gmail.com>
2022-07-11 13:31:29 +0200

    management of trackid

--- App/queries.py
+++ App/queries.py
@@ -10,15 +10,42 @@ def getTracksByFeatures(n: int, dance: str, energy: str, valence: str):
     return result
 
 
-
-def getArtistFromTrack(tracks: list, album: list):
+def getTracksName(trackids: list):
     pi = kbAccess.PrologInterface()
-    pi.set_to_consult(['track.pl', 'album_contains.pl', 'utils.pl', 'init.pl', 'published_by.pl', 'album.pl'])
+    pi.set_to_consult(['track.pl', 'utils.pl', 'init.pl',  'tracksuggest.pl'])
     pi.load_rules()
-    query = f"retrieveAllArtists({tracks}, {album}, Artists)."
+    query = f"getTrackName({trackids}, Tracks)."
+    result = pi.query(query)
+    return result
+
+
+def retrieveArtistsByID(tracks: list):
+    pi = kbAccess.PrologInterface()
+    pi.set_to_consult(['album_contains.pl', 'utils.pl', 'init.pl', 'published_by.pl', 'album.pl'])
+    pi.load_rules()
+    query = f"retrieveArtistsByID({tracks}, Artists)."
     result = pi.query(query)
     return result
 
 
 if __name__ == '__main__':
-    print(getArtistFromTrack(["angela", "boogie woogie baltimore"], ["cleopatra", "null"]))
+
+
+    trackIds = getTracksByFeatures(10, "high_danceable", "high_energy", "low_valence")[0]['Tracks']
+    tracksName = [tracks.replace("-", "").title() for tracks in getTracksName(trackIds)[0]['Tracks']]
+    Artists = [artist.title() for artist in retrieveArtistsByID(trackIds)[0]['Artists']]
+    result_string = list(map(' - '.join, zip(tracksName, Artists)))
+    dict_tracks = {k: v for k, v in zip(result_string, trackIds)}
+
+
+    # così si mostrano tutte le canzoni con i relativi artisti
+    print(dict_tracks.keys())
+
+
+    mykeys = (list(dict_tracks.keys())[1:3])
+
+    # cosi si ritornano gli id solo per alcune tracce (quelle scelte dall'utente
+    print([dict_tracks[x] for x in mykeys])
+
+
+
